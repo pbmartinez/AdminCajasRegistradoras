79e886240c90f3aca015e7570261a5fb9360c32b
Pablo Bárbaro Martinez Pedroso <pmartinezpedroso@gmail.com>
2021-12-09 16:08:46 -0500

    Add project files.

---
+++ AdminCajasRegistradoras/Models/Caja.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminCajasRegistradoras.Models
+{
+    public class Caja
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+
+        public int Numero { get; set; }
+
+        [Required]
+        public string Ip { get; set; } = string.Empty;
+
+    }
+}
